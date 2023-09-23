d037cdf5c5f273a7db656cdc138ffb81300504c3
Chaitanya <chaitu.vsh@gmail.com>
2023-09-23 13:23:32 +0530

    [BASICS]: Learned props, varaibles, css variables, clickHandlers, conditional rendering, loops etc.

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,52 @@
 <script>
-	export let name;
+	let firstName = "Chaitu"
+	let lastName = "Maverick"
+	$: name = firstName + " " + lastName;
+	
+	let color = "orange";
+	let showText = false;
+
+	let todos = [
+		{
+			id: 1,
+			task: "Learn svelte"
+		},
+		{
+			id: 2,
+			task: "Write test cases"
+		},
+		{
+			id: 3,
+			task: "Raise CRs"
+		}
+	];
+
+	const clickHandler = () => {
+		// alert("Clicked")
+		color = (color == "red") ? "blue" : "red";
+		showText = !showText;
+		todos = [...todos, {
+			id: 4,
+			task: "Wash clothes"
+		}]
+	}
+
+	
+
+	console.log("Yayyy!");
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<h1>Hello <span style="color: {color}">{name}!</span></h1>
+	{#if showText}
+		<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	{:else}
+		<p>No text</p>
+	{/if}
+	<button on:click={() => clickHandler()}>Click me !</button>
+	{#each todos as todo}
+		<p>{todo.id}: {todo.task}</p>
+	{/each}
 </main>
 
 <style>

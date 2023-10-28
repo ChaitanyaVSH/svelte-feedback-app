8c908fc386d7b1e458b50cb9049348de19bcc246
Chaitanya <chaitu.vsh@gmail.com>
2023-10-28 21:58:40 +0530

    [TRANSITIONS] Adding basic effects

--- src/components/FeedbackList.svelte
+++ src/components/FeedbackList.svelte
@@ -1,8 +1,14 @@
 <script>
+    import { fade, scale } from 'svelte/transition';
     import FeedbackItem from "./FeedbackItem.svelte";
     export let feedbacks = [];
 </script>
 
 {#each feedbacks as feedback (feedback.id)}
-    <FeedbackItem item={feedback} on:delete-handler/>
+    <div
+        in:scale
+        out:fade={{ delay: 0, duration: 300}}
+    >
+        <FeedbackItem item={feedback} on:delete-handler/>
+    </div>
 {/each}
